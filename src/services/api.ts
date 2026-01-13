6552d3de92dec16cf639ab248bdd0832da584c1c
AdrianZapico <joeytafolla123@gmail.com>
2026-01-13 12:55:15 -0300

    Configurando URLs de produção

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "/api",
 });
 
 // Interceptor para adicionar o token JWT em todas as requisições
